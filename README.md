a453361a64bee89d38eeeae9392dc235604cbd9d
David Cole <davidtcole@yahoo.com>
2020-08-19 10:35:24 -0700

    build out the UI for the leap year project in our index.html file.
    Co-authored by: David McCaslin <davidmccaslin94@gmail.com>

--- README.md
+++ README.md
@@ -10,7 +10,12 @@ _{This is a detailed description of your application. Its purpose and usage.  Gi
 
 ## Specs
 
-__
+_Describe: isLeapYear()
+ Test: "It returns false for years that are not a leap year"
+ Expect(isLeapYear(1993)).toEqual(false);
+
+ Test: "It returns true for years that are divisible by 4"
+ Expect(isLeapYear(2004)).toEqual(true);_
 
 ## Setup/Installation Requirements
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-event.preventDefault();
+  $("#form").submit(function(event) {
+    event.preventDefaults();
+    const userInput = parseInt($("input#yearInput").val());
+    if (isLeapYear(userInput)) {
+      
+    }
+  });
 })
+
+//business logic
+function isLeapYear(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
